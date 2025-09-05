bed5cd8ecb22d3d61cec7dd5d6fd3316b83b14a8
Rafael de Aquino <rafaeldeaquinoy@gmail.com>
2025-09-05 16:48:28 -0300

    Add files via upload

--- primeiroProjeto/src/primeiroProjeto/teste.java
+++ primeiroProjeto/src/primeiroProjeto/teste.java
@@ -4,19 +4,82 @@ import java.util.Scanner;
 
 public class teste {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
 
-			Scanner	scanner = new Scanner(System.in);
+        // 6 - Tipo de Ingresso
+        System.out.println("Digite o código do ingresso (1=Inteira, 2=Meia-entrada, 3=VIP): ");
+        int ingresso = scanner.nextInt();
 
-			System.out.print("Digite uma letra: ");
-	        char letra = scanner.next().toLowerCase().charAt(0);
+        switch (ingresso) {
+            case 1:
+                System.out.println("Preço: R$50,00");
+                break;
+            case 2:
+                System.out.println("Preço: R$25,00");
+                break;
+            case 3:
+                System.out.println("Preço: R$100,00");
+                break;
+            default:
+                System.out.println("Ingresso inválido");
+        }
 
-	        if (letra == 'a') {
-	            System.out.println("É uma vogal.");
-	        } else {
-	            System.out.println("Não é vogal.");
-	        }
-	}
+        // 7 - Escolha de Idioma
+        System.out.println("\nDigite o código do idioma (1=Português, 2=Inglês, 3=Espanhol): ");
+        int idioma = scanner.nextInt();
 
+        switch (idioma) {
+            case 1:
+                System.out.println("Saudação: Olá!");
+                break;
+            case 2:
+                System.out.println("Saudação: Hello!");
+                break;
+            case 3:
+                System.out.println("Saudação: Hola!");
+                break;
+            default:
+                System.out.println("Idioma inválido");
+        }
+
+        // 8 - Nível de Acesso
+        System.out.println("\nDigite o código do nível (1=Usuário, 2=Moderador, 3=Administrador): ");
+        int nivel = scanner.nextInt();
+
+        switch (nivel) {
+            case 1:
+                System.out.println("Permissões: Acesso básico");
+                break;
+            case 2:
+                System.out.println("Permissões: Acesso básico + edição");
+                break;
+            case 3:
+                System.out.println("Permissões: Acesso total");
+                break;
+            default:
+                System.out.println("Nível inválido");
+        }
+
+        // 9 - Tipo de Pagamento
+        System.out.println("\nDigite o código de pagamento (1=Dinheiro, 2=Cartão de Crédito, 3=Pix): ");
+        int pagamento = scanner.nextInt();
+
+        switch (pagamento) {
+            case 1:
+                System.out.println("Pagamento em dinheiro confirmado");
+                break;
+            case 2:
+                System.out.println("Pagamento com cartão processado");
+                break;
+            case 3:
+                System.out.println("Pagamento via Pix realizado");
+                break;
+            default:
+                System.out.println("Método inválido");
+        }
+
+        scanner.close();
+    }
 }
